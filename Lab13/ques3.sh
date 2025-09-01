00ad5d3038f784f7264b1556f12838c94541711c
inshikathakur <inshikathakur511@gmail.com>
2025-09-01 09:28:15 +0530

    Lab 13

---
+++ Lab13/ques3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#i)Using gawk remove the lines containing >.
+#gawk '!/^>/' ques3.txt
+
+#ii)Using sed convert the DNA sequence to RNA sequence.
+#sed 's/T/U/g' ques3.txt
+
+#iii)Using sed replace seq1 (called the sequence ID) by human_gene.
+#sed 's/seq1/human_gene/g' ques3.txt
