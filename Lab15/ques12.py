3dc142dd5b8a22bdf0f9283df7a1df90455d88d3
Inshika Thakur <inshikathakur511@gmail.com>
2025-09-28 22:19:22 +0530

    Update ques12.py

--- Lab15/ques12.py
+++ Lab15/ques12.py
@@ -2,4 +2,10 @@
 
 #Write a program to check if an input string is palindrome
 
-
+A=input("Enter a word: ")
+B=(A[::-1])
+print(f'{B}')
+if A==B:
+  print("The word that you entered is a plaindrome")
+else:
+  print("The word that you entered is not a palindrome")
